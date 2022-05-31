ba14e157c9a8f40bc1e6dca24f6a50f84d4de99f
graca0 <graca333@interia.eu>
2022-05-31 19:14:59 +0200

    poprawienie kulek

--- RayTracing/RayTracing/Class1.cs
+++ RayTracing/RayTracing/Class1.cs
@@ -18,32 +18,34 @@ namespace RayTracing
             IMaterial grayMat = new PerfectOne(Color.Gray);
             IMaterial yellowMaT = new PerfectOne(Color.Yellow);
             IMaterial whiteMat = new PerfectOne(Color.White);
+            IMaterial hotPinkMat = new PerfectOne(Color.HotPink);
             IMaterial blackMat = new PerfectOne(Color.Black);
 
             World world = new World(Color.LightBlue);
 
-            world.Add(new Sphere(new Vector(0, 4, 15), 5, whiteMat));
-            world.Add(new Sphere(new Vector(0, 0, 11), 3, yellowMaT));
-            world.Add(new Sphere(new Vector(0, -2, 9), 2, whiteMat));
-            world.Add(new Sphere(new Vector(1, 0.5, 8), 0.2, blackMat));
-            world.Add(new Sphere(new Vector(-1, 0.5, 8), 0.2, blackMat));
-            world.Add(new Plane(new Vector(0, 2, 0), new Vector(0, 2, 0), grayMat));
-            world.AddLight(new LightTypePoint(new Vector(0, 5, -5), Color.White));
+            world.Add(new Sphere(new Vector(-4, 0, 0), 3, whiteMat));
+            world.Add(new Sphere(new Vector(4, 1, 0), 2, hotPinkMat));
+            world.Add(new Sphere(new Vector(-4, 0, 0), 3, whiteMat));
+            world.Add(new Sphere(new Vector(4, 0, 0), 3, yellowMaT));
+            world.Add(new Sphere(new Vector(0, 0, 3), 2, whiteMat));
+            
+            world.Add(new Plane(new Vector(0, -2, 0), new Vector(0, 1, 0), grayMat));
+            world.AddLight(new LightTypePoint(new Vector(5, 5, -5), Color.White));
 
             /* world.Add(new Sphere(new Vector(-4, 0, 0), 2, Color.Red));
              world.Add(new Sphere(new Vector(4, 0, 0), 2, Color.Green));
              world.Add(new Sphere(new Vector(0, 0, 3), 2, Color.Blue));*/
 
             //ICamera camera = new OrtogonalCamera(new Vector(0, 0, -5), 0, new Vector(5, 5));
-            ICamera camera = new Pinhole(new Vector(3, 15, -1), new Vector(0, 0, 0), new Vector(0, -1, 0), 1);
+            ICamera camera = new Pinhole(new Vector(0,-1, -10), new Vector(0, 0, 0), new Vector(0, -1, 0), 1);
             Raytracer tracer = new Raytracer();
           
-            Bitmap image = tracer.RayTrace(world, camera, new Size(256, 256));
+            Bitmap image = tracer.RayTrace(world, camera, new Size(2532,2543));
            
             Console.WriteLine("Start...");
-            //image.Save("C:\\Users\\Graca\\Documents\\GitHub\\test2.png"); //Graca
+            image.Save("C:\\Users\\Graca\\Documents\\GitHub\\test3.png"); //Graca
            // image.Save("D:\\STUDIA\\Semestr VI\\PRIR\\Projekt\\test2.png"); //Slawek
-           image.Save("C:\\Users\\Dawid\\Desktop\\szkolka\\PK\\RayTracingProject\\RayTracing\\test2.png");// Dawid
+           //image.Save("C:\\Users\\Dawid\\Desktop\\szkolka\\PK\\RayTracingProject\\RayTracing\\test2.png");// Dawid
 
             Console.WriteLine("Koniec");
         }
